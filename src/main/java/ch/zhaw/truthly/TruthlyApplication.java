42c87505895416160f68fda32893d4812762f165
ericstrausak <straueri@students.zhaw.ch>
2025-03-05 14:45:44 +0100

    first commit

---
+++ src/main/java/ch/zhaw/truthly/TruthlyApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.truthly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruthlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruthlyApplication.class, args);
+	}
+
+}
