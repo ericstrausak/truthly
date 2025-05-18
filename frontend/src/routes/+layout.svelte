86565df680a8f3738a5d330e819ffa09f097d1c0
ericstrausak <straueri@students.zhaw.ch>
2025-05-18 21:52:05 +0200

    added changes to the navbar. closes #28

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -7,12 +7,30 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">Truthly</a>
-    <!-- ... navbar toggle button ... -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         {#if $isAuthenticated}
           <li class="nav-item">
             <a class="nav-link" href="/articles">Articles</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/users">Users</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/factchecks">Fact Checks</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/account">Account</a>
           </li>
         {/if}
       </ul>
