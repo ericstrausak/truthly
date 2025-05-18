c86e11c0a1fde7bb61eb1ca1a8d0e15f10508e77
ericstrausak <straueri@students.zhaw.ch>
2025-05-18 17:35:35 +0200

    added login to backend

---
+++ frontend/src/routes/account/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { user } from "../../store";
+</script>
+
+{#if $user}
+  <h1>Account Details</h1>
+  <p><strong>Name:</strong> {$user.name}</p>
+  <p><strong>Email:</strong> {$user.email}</p>
+  <p><strong>Nickname:</strong> {$user.nickname}</p>
+  <!-- Add more user details as needed -->
+{/if}
